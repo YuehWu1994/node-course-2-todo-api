da60a0202ab4545b883b4b3dc9df0f75a408615b
Apple <rex9999772001@gmail.com>
2018-10-30 19:50:42 -0700

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: 'first todos'
+}, {
+  text: 'second todos'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(()=> done());
+  Todo.remove({}).then(()=> {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe(('post todo'), ()=>{
@@ -25,7 +33,7 @@ describe(('post todo'), ()=>{
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -45,9 +53,21 @@ describe(('post todo'), ()=>{
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       })
+  });
+});
+
+describe(('GET /todos'), ()=> {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) =>{
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done)
   });
 });
