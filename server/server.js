228d82e94259d96ebce185d35dc0ea791e50247c
Apple <rex9999772001@gmail.com>
2018-10-30 20:52:57 -0700

    add get todo id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
@@ -27,6 +29,24 @@ app.get('/todos', (req, res) => {
    }, (e) => {
      res.status(400).send(e);
    })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // valid id using isValid
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, ()=> {
