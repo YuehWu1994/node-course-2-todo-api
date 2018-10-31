65ed3553f0dcc88763075f588ef9cc8aeeec55ca
Apple <rex9999772001@gmail.com>
2018-10-31 13:15:31 -0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {mongoose} = require('./db/mongoose');
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
@@ -66,7 +67,33 @@ app.delete('/todos/:id', (req, res) => {
   }, (e) => {
     res.status(400).send();
   });
+});
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // pick some part to update
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, ()=> {
