2c20a65d63f7a367020be355342450d62a40aeff
Apple <rex9999772001@gmail.com>
2018-11-01 16:53:00 -0700

    Hashing user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
 
-var data = {
-  id: 10
-};
+var password = 'abc123';
 
-var token = jwt.sign(data, '123455');
-//console.log(token);
-var decode = jwt.verify(token,'123455');
-console.log(decode);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
+var hashPassword = '$2a$10$5g2AfK4bygI1xExQlyV8G.3EZ3adWOVyzjchkUqh17LqXbk45X7Sy';
+
+bcrypt.compare(password, hashPassword, (err, res) => {
+  console.log(res);
+})
+
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123455');
+// //console.log(token);
+// var decode = jwt.verify(token,'123455');
+// console.log(decode);
+//
 
 
 
